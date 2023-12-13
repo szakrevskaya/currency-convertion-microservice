023aba95c4cea31defb801129c0696fd44b9c22a
Snezhana Zakrevskaya <snezhana@billmybank.com>
2023-12-13 17:31:28 +0300

    Updates for deploy

--- src/main/java/com/example/currencyconversionservice/model/CurrencyConversionDto.java
+++ src/main/java/com/example/currencyconversionservice/model/CurrencyConversionDto.java
@@ -12,11 +12,9 @@ import java.math.BigDecimal;
 @Builder
 @Data
 public class CurrencyConversionDto {
-    private Integer id;
     private String from;
     private String to;
     private BigDecimal conversionMultiple;
     private BigDecimal quantity;
     private BigDecimal totalCalculatedAmount;
-    private int port;
 }
--- src/main/java/com/example/currencyconversionservice/service/CurrencyConvertionService.java
+++ src/main/java/com/example/currencyconversionservice/service/CurrencyConvertionService.java
@@ -1,30 +1,58 @@
 package com.example.currencyconversionservice.service;
 
 import com.example.currencyconversionservice.model.CurrencyConversionDto;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
-import java.util.HashMap;
-import java.util.Map;
+import java.math.RoundingMode;
 
 @Service
 public class CurrencyConvertionService {
-    private static String FOREX_SERVICE_URL = "http://localhost:8000/ce/from/{from}/to/{to}";
+
+    private static final String USD = "USD";
+    private static final String EUR = "EUR";
+    private static final String BYR = "BYR";
+    private static final BigDecimal USD_CONVERSION_MULTIPLY = BigDecimal.valueOf(3.162);
+    private static final BigDecimal EUR_CONVERSION_MULTIPLY = BigDecimal.valueOf(3.412);
 
     public CurrencyConversionDto convertCurrency(String from, String to, BigDecimal quantity) {
-        Map<String, String> uriVariables = new HashMap<>();
-        uriVariables.put("from", from);
-        uriVariables.put("to", to);
+        BigDecimal conversionMultiple = BigDecimal.valueOf(3.162);
+        if (from.equals(USD) && to.equals(BYR)) {
+            return CurrencyConversionDto.builder()
+                    .from(USD)
+                    .to(BYR)
+                    .quantity(quantity)
+                    .conversionMultiple(USD_CONVERSION_MULTIPLY)
+                    .totalCalculatedAmount(quantity.multiply(conversionMultiple)).build();
+        }
 
-        ResponseEntity<CurrencyConversionDto> responseEntity = new RestTemplate().getForEntity(
-                FOREX_SERVICE_URL, CurrencyConversionDto.class,
-                uriVariables);
-        CurrencyConversionDto response = responseEntity.getBody();
-        response.setQuantity(quantity);
-        response.setTotalCalculatedAmount(quantity.multiply(response.getConversionMultiple()));
+        if (from.equals(EUR) && to.equals(BYR)) {
+            return CurrencyConversionDto.builder()
+                    .from(EUR)
+                    .to(BYR)
+                    .quantity(quantity)
+                    .conversionMultiple(EUR_CONVERSION_MULTIPLY)
+                    .totalCalculatedAmount(quantity.multiply(conversionMultiple)).build();
+        }
 
-       return response;
+        if (from.equals(BYR) && to.equals(USD)) {
+            return CurrencyConversionDto.builder()
+                    .from(BYR)
+                    .to(USD)
+                    .quantity(quantity)
+                    .conversionMultiple(USD_CONVERSION_MULTIPLY)
+                    .totalCalculatedAmount(quantity.divide(conversionMultiple, RoundingMode.HALF_UP)).build();
+        }
+
+        if (from.equals(BYR) && to.equals(EUR)) {
+            return CurrencyConversionDto.builder()
+                    .from(BYR)
+                    .to(EUR)
+                    .quantity(quantity)
+                    .conversionMultiple(EUR_CONVERSION_MULTIPLY)
+                    .totalCalculatedAmount(conversionMultiple.divide(quantity, RoundingMode.HALF_UP)).build();
+        }
+
+        return new CurrencyConversionDto();
     }
 }
