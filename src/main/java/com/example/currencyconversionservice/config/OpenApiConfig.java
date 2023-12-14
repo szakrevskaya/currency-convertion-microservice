e5176f322e88dd4283436b31d9c313cd932f638a
Snezhana Zakrevskaya <snezhana@billmybank.com>
2023-12-14 15:44:32 +0300

    Commit1

--- src/main/java/com/example/currencyconversionservice/config/OpenApiConfig.java
+++ src/main/java/com/example/currencyconversionservice/config/OpenApiConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
+//Some small changs
 @Configuration
 public class OpenApiConfig {
     @Bean
